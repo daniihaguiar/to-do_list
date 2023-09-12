7bd0caba29cc874fab2bc15ce290d4a20e05606c
Danih <danieleaguiar272@gmail.com>
2023-09-12 16:36:20 -0300

    update da lista de tarefas

---
+++ js/arrays.js
@@ -0,0 +1,33 @@
+// Um array sem elementos
+let empty = [];
+
+// Um array com 5 elemnetos numéricos 
+let primes = [2, 3, 5, 7, 11];
+
+// 3 elementos de vários tipos + vírgula
+let misc = [1.1, true, "a"];
+
+//Chamada sem argumentos 
+var a = new Array();
+
+//Chamada com um único argumento numérico, o qual especifica um comprimento
+var b = new Array(10);
+
+//Especificação explícita dde dois ou mais elementos de array ou de apenas um elemento não númerico para o array
+var c = new Array(5, 4, 3, 2, 1, "testing, testing");
+
+//Começa com um array de um elemento
+var a = ["world"];
+
+//Lê o elemento 0
+var value = a[0];
+
+//Grava o elemento 1
+a[1] = 3.14;
+
+//Grava o elemento 2
+i = 2;
+a[i] = 3;
+
+//Grava o elemento 3
+a[i + 1] = "hello";
